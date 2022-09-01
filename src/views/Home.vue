3baa1302c4b6019bd1bd8e3929a4f0280548bbc4
codercypt <56701242+codercypt@users.noreply.github.com>
2022-09-01 13:11:01 -0300

    1st

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-content>
+    <Hero />
+    <Gallery />
+    <LatestPost />
+  </v-content>
+</template>
+
+<script>
+import Hero from "../components/Hero";
+import Gallery from "../components/Gallery";
+import LatestPost from "../components/LatestPost";
+
+export default {
+  name: "Home",
+//renders hero gallery etc
+  components: {
+    Hero,
+    Gallery,
+    LatestPost
+  }
+};
+</script>
